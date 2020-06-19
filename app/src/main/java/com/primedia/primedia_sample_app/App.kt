391ed07420e25eed1df126ac83220dcdfffdd8ce
larry stent <lawrencestent@gmail.com>
2020-06-19 14:44:28 +0200

    remove unneeded dependency injections

--- app/src/main/java/com/primedia/primedia_sample_app/App.kt
+++ app/src/main/java/com/primedia/primedia_sample_app/App.kt
@@ -3,13 +3,9 @@ package com.primedia.primedia_sample_app
 import android.app.Application
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
-import com.primedia.primedia_sample_app.rest.PrimediaRepository
 import com.primedia.primedia_sample_app.rest.RetrofitService
 import com.primedia.primedia_sample_app.room.AppDatabase
 import com.primedia.primedia_sample_app.room.repositories.MediaRepository
-import com.primedia.primedia_sample_app.room.repositories.PlaylistMediaLinkRepository
-import com.primedia.primedia_sample_app.room.repositories.PlaylistRepository
-import com.primedia.primedia_sample_app.service.PreferenceService
 import com.primedia.primedia_sample_app.triton.Player
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
@@ -31,28 +27,10 @@ class App: Application() {
 
         kodein = Kodein {
             bind<Context>() with instance(this@App)
-//            bind<AuthService>() with singleton {
-//                AuthService(
-//                    instance(),
-//                    instance(),
-//                    instance(),
-//                    instance(),
-//                    instance(),
-//                    instance(),
-//                    instance()
-//                )
-//            }
-//            bind<AnalyticsService>() with singleton { AnalyticsService(instance(), instance(), instance()) }
-            bind<RetrofitService>() with singleton { RetrofitService(instance(), instance()) }
-            bind<PrimediaRepository>() with singleton { PrimediaRepository(instance()) }
-            bind<Player>() with singleton { Player(instance(), instance(), instance(), instance(), instance(), instance(), instance()) }
-            bind<PreferenceService>() with singleton { PreferenceService(instance()) }
+            bind<RetrofitService>() with singleton { RetrofitService(instance()) }
+            bind<Player>() with singleton { Player(instance(), instance(), instance()) }
             bind<AppDatabase>() with instance(AppDatabase.getInstance(this@App) as AppDatabase)
-//            bind<SearchRepository>() with provider { SearchRepository(instance()) }
             bind<MediaRepository>() with provider { MediaRepository(instance()) }
-            bind<PlaylistRepository>() with provider { PlaylistRepository(instance()) }
-            bind<PlaylistMediaLinkRepository>() with provider { PlaylistMediaLinkRepository(instance()) }
-//            bind<OfflineService>() with provider { OfflineService(instance()) }
         }
 
         Timber.plant(Timber.DebugTree())
